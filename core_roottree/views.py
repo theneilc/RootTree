79e5148b9875d0f042471a487c0f573dc7dd0867
pranavbadami <pranavbadami@gmail.com>
2014-09-12 22:22:59 -0400

    added api view skeleton

--- core_roottree/views.py
+++ core_roottree/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. This is roottree")
+
+class TaskAPIView(APIView):
+	def post(self, request):
+		# get task url
+		# return ID
+		return Response()
+
+class ClientPollAPIView(APIView):
+	def get(self, request):
+		# return task
+		return Response()
+
+class ClientDoneAPIView(APIView):
+	def post(self, request):
+		# if file upload to s3 (through model method?)
+		return Response()
+	
+	def get(self, request):
+		# long polling from library 
+		return Response()
