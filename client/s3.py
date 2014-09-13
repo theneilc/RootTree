134df4c94dcf96228f967eada84b681ba0fad7ee
Sanket Chauhan <sanke93@gmail.com>
2014-09-13 14:23:46 -0400

    updated uploading to s3 to include meta file

--- client/s3.py
+++ client/s3.py
@@ -1,38 +1,60 @@
 import os
 import requests
 
-policy = 'eyJleHBpcmF0aW9uIjogIjIwMTUtMDEtMDFUMDA6MDA6MDBaIiwKICAiY29uZGl0aW9ucyI6IFsgCiAgICB7ImJ1Y2tldCI6ICJyb290dHJlZWJ1Y2tldCJ9LCAKICAgIFsic3RhcnRzLXdpdGgiLCAiJGtleSIsICIiXSwKICAgIHsiYWNsIjogInB1YmxpYy1yZWFkIn0sCiAgXQp9'
-signature = 'cppCKmmC45scE+EPLGl3idSn7/A='
 AWSAccessKeyId = 'AKIAJQE2SYERMZG7CL5Q'
 
-def upload_file(filepath=None, content=None):
+def upload_file(policy, signature, uuid, filepath=None, content=None):
 	url = 'https://roottreebucket.s3.amazonaws.com/'
 	payload = {
 				'key': '${filename}',
 				'AWSAccessKeyId': 'AKIAJQE2SYERMZG7CL5Q',
 				'acl': 'public-read',
 				'policy': policy,
-				'signature': signature
+				'signature': signature,
 			   }
-	files = {'file': open(filepath, 'r')}
-	r = requests.post(url, data=payload, files=files)
-	print r.content
-	return r.headers
 
-def get_file(url):
-	url = url
-	payload = {'AWSAccessKeyId': AWSAccessKeyId,
-				'Signature': signature
-				}
-	print payload
-	r = requests.get(url, data=payload)
-	print r.url
-	return r.headers
+	url_list = {}
+	#rename the file, and open it
+	if filepath is not None:
+		base = os.path.basename(filepath)
+		original_filename = os.path.splitext(base)[0]
+		extension = os.path.splitext(base)[1]
+		directory = os.path.dirname(filepath)
+		new_filepath = directory + '/' + uuid + extension
+		os.rename(filepath, new_filepath)
+		files_temp = open(new_filepath, 'r')
+		files = {'file': files_temp}
+		r = requests.post(url, data=payload, files=files)
+		os.rename(new_filepath, filepath)
+		files_temp.close()
+		url_list['file_url'] = r.headers['location']
+
+	if content is not None:
+		print content
+		metafile_name = uuid + '_meta.txt'
+		metafile = open(metafile_name, 'w')
+		metafile.write(content)
+		metafile.close()
+		metafile = open(metafile_name, 'r')
+		files = {'file': metafile}
+		r = requests.post(url, data=payload, files=files)
+		metafile.close()
+		url_list['result_url'] = r.headers['location']
+	
+	return url_list
+
 
 if __name__ == "__main__":
-	filepath = '/Users/sanketchauhan/Downloads/hi.txt'
-	#print upload_file(filepath=filepath)
-	print get_file(url='https://roottreebucket.s3.amazonaws.com/hi.txt')
-
-#if not an image return contents
-#uuid.uuid4().hex
+	filepath = '/Users/sanketchauhan/Downloads/sanke93.jpg'
+	# policy = 'eyJleHBpcmF0aW9uIjogIjIwMTUtMDEtMDFUMDA6MDA6MDBaIiwKICAiY29uZGl0aW9ucyI6IFsgCiAgICB7ImJ1Y2tldCI6ICJyb290dHJlZWJ1Y2tldCJ9LCAKICAgIFsic3RhcnRzLXdpdGgiLCAiJGtleSIsICIiXSwKICAgIHsiYWNsIjogInB1YmxpYy1yZWFkIn0sCiAgXQp9'
+	# signature = 'cppCKmmC45scE+EPLGl3idSn7/A='
+	AWS_SECRET_ACCESS_KEY = 'CVdcUQd3jQXmHK5aaq5yrfYR+tdfYrRMF7M4UVFV'
+	import base64
+	import hmac, hashlib
+	policy_document = open('policy_document.json', 'r').read()
+	policy = base64.b64encode(policy_document)
+	signature = base64.b64encode(hmac.new(AWS_SECRET_ACCESS_KEY, policy, hashlib.sha1).digest())
+	uuid = 'thisworks'
+	hi = upload_file(policy=policy, signature=signature, uuid=uuid, filepath=filepath, content='this is content')
+	print hi
+	#print get_file(url='https://roottreebucket.s3.amazonaws.com/hi.txt')
