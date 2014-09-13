a47c5e46b7d63caebecafaa53df30aeaecadf901
Sanket Chauhan <sanke93@gmail.com>
2014-09-13 19:39:07 -0400

    updated fixtures

--- scripts/fixture_approved.py
+++ scripts/fixture_approved.py
@@ -6,8 +6,11 @@ commands = {
 	'Install brew': ['ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"', 'b'],
 	'Install npm': ['curl -O -L https://npmjs.org/install.sh; sudo sh install.sh', 'b'],
 	'Install node': ['ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; brew install node', 'b'],
-	'Install clumsy-bird': ['npm install; grunt connect; open http://localhost:8001/', 'b']
+	'Install clumsy-bird': ['git clone https://github.com/ellisonleao/clumsy-bird.git; npm install; grunt connect; open http://localhost:8001/', 'b']
+	'Upload sketch': ['pip install ino; ', 'b'],
+	'Notify desktop': ['osascript -e \'display notification \"%s\" with title \"%s\"\'', 'b']
 }
 
 for key,val in commands.iteritems():
 	Command.objects.create(name=key, code=val[0], language=val[1])
+
