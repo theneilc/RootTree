b7cd793a2678823a656bb72059d34290d221d20d
Neil <theneilc@gmail.com>
2014-09-13 09:57:10 -0400

    included setup and test client file. can be built and tested

---
+++ client/RootTree.py
@@ -0,0 +1,21 @@
+"""
+Description:
+
+Author:
+    Neil C
+
+Usage:
+    python [filename]
+
+Python Version:
+    2.7.5 
+
+Notes:
+
+"""
+#==============================================================================
+
+
+f = open('hello_there.txt', 'w+')
+f.write('nice!')
+f.close()
