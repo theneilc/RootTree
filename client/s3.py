3970e962070184fd2afc6e918cff474e620b2d08
Sanket Chauhan <sanke93@gmail.com>
2014-09-13 12:00:50 -0400

    updated s3 to public-read

--- client/s3.py
+++ client/s3.py
@@ -1,22 +1,38 @@
 import os
 import requests
 
-policy = 'eyJleHBpcmF0aW9uIjogIjIwMTUtMDEtMDFUMDA6MDA6MDBaIiwKICAiY29uZGl0aW9ucyI6IFsgCiAgICB7ImJ1Y2tldCI6ICJyb290dHJlZWJ1Y2tldCJ9LCAKICAgIFsic3RhcnRzLXdpdGgiLCAiJGtleSIsICIiXSwKICAgIHsiYWNsIjogImF1dGhlbnRpY2F0ZWQtcmVhZCJ9LAogIF0KfQ=='
-signature = 'PWrxSaAOYfnLe7q/6tCVXwfaQw8='
+policy = 'eyJleHBpcmF0aW9uIjogIjIwMTUtMDEtMDFUMDA6MDA6MDBaIiwKICAiY29uZGl0aW9ucyI6IFsgCiAgICB7ImJ1Y2tldCI6ICJyb290dHJlZWJ1Y2tldCJ9LCAKICAgIFsic3RhcnRzLXdpdGgiLCAiJGtleSIsICIiXSwKICAgIHsiYWNsIjogInB1YmxpYy1yZWFkIn0sCiAgXQp9'
+signature = 'cppCKmmC45scE+EPLGl3idSn7/A='
+AWSAccessKeyId = 'AKIAJQE2SYERMZG7CL5Q'
 
-def upload_file(filepath):
+def upload_file(filepath=None, content=None):
 	url = 'https://roottreebucket.s3.amazonaws.com/'
 	payload = {
 				'key': '${filename}',
 				'AWSAccessKeyId': 'AKIAJQE2SYERMZG7CL5Q',
-				'acl': 'authenticated-read',
+				'acl': 'public-read',
 				'policy': policy,
 				'signature': signature
 			   }
 	files = {'file': open(filepath, 'r')}
 	r = requests.post(url, data=payload, files=files)
+	print r.content
+	return r.headers
+
+def get_file(url):
+	url = url
+	payload = {'AWSAccessKeyId': AWSAccessKeyId,
+				'Signature': signature
+				}
+	print payload
+	r = requests.get(url, data=payload)
+	print r.url
 	return r.headers
 
 if __name__ == "__main__":
 	filepath = '/Users/sanketchauhan/Downloads/hi.txt'
-	print upload_file(filepath=filepath)
+	#print upload_file(filepath=filepath)
+	print get_file(url='https://roottreebucket.s3.amazonaws.com/hi.txt')
+
+#if not an image return contents
+#uuid.uuid4().hex
