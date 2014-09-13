47eb8f2738d48e0b4a302cdee06bc95769d68077
Alice Zheng <evolutia2001@gmail.com>
2014-09-12 21:58:47 -0400

    added db.sqlite3 to .gitignore, added some more setup for restframework

--- roottree/urls.py
+++ roottree/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework import routers
 
-urlpatterns = patterns('',
+router = routers.DefaultRouter()
+
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'roottree.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^test$', 'core_roottree.views.index'),
+    url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
