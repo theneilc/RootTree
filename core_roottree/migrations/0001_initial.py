27e220f9363487f7d301852de695e969696468e3
Alice Zheng <evolutia2001@gmail.com>
2014-09-12 22:30:34 -0400

    most of the models

---
+++ core_roottree/migrations/0001_initial.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+from django.conf import settings
+import django_extensions.db.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ClientUser',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('uuid', models.CharField(max_length=32)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Command',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('code', models.TextField()),
+            ],
+            options={
+                'ordering': ('-modified', '-created'),
+                'abstract': False,
+                'get_latest_by': 'modified',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='CommandInstance',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('args', models.TextField()),
+                ('kwargs', models.TextField()),
+                ('command', models.ForeignKey(to='core_roottree.Command')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Developer',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Service',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('lastrun', models.DateTimeField()),
+                ('frequency', models.IntegerField()),
+                ('commandinstance', models.OneToOneField(to='core_roottree.CommandInstance')),
+            ],
+            options={
+                'ordering': ('-modified', '-created'),
+                'abstract': False,
+                'get_latest_by': 'modified',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Session',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('uuid', models.CharField(max_length=32)),
+                ('status', models.CharField(max_length=1, choices=[('N', 'Not Requested'), ('P', 'Pending'), ('C', 'Complete')])),
+                ('result', models.TextField(null=True, blank=True)),
+                ('callback_url', models.URLField(null=True, blank=True)),
+                ('client', models.ForeignKey(to='core_roottree.ClientUser')),
+                ('commandinstance', models.ForeignKey(to='core_roottree.CommandInstance')),
+                ('developer', models.ForeignKey(to='core_roottree.Developer')),
+            ],
+            options={
+                'ordering': ('-modified', '-created'),
+                'abstract': False,
+                'get_latest_by': 'modified',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Task',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, verbose_name='created', editable=False, blank=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, verbose_name='modified', editable=False, blank=True)),
+                ('commandinstance', models.OneToOneField(to='core_roottree.CommandInstance')),
+            ],
+            options={
+                'ordering': ('-modified', '-created'),
+                'abstract': False,
+                'get_latest_by': 'modified',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='command',
+            name='owner',
+            field=models.ForeignKey(blank=True, to='core_roottree.Developer', null=True),
+            preserve_default=True,
+        ),
+    ]
--- core_roottree/models.py
+++ core_roottree/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ClientUser(models.Model):
+    uuid = models.CharField(max_length=32)
+    # email = models.EmailField(unique=True, max_length=254)
+    user = models.OneToOneField(User)
+
+
+class Developer(models.Model):
+    # email = models.EmailField(unique=True, max_length=254)
+    user = models.OneToOneField(User)
+
+
+class Session(TimeStampedModel):
+    uuid = models.CharField(max_length=32)
+    client = models.ForeignKey(ClientUser)
+    developer = models.ForeignKey(Developer)
+
+    STATUS_CHOICES = (
+        (u'N', u'Not Requested'),
+        (u'P', u'Pending'),
+        (u'C', u'Complete'),
+    )
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    result = models.TextField(null=True, blank=True)
+    callback_url = models.URLField(null=True, blank=True)
+    commandinstance = models.ForeignKey('CommandInstance')
+
+
+class Service(TimeStampedModel):
+    commandinstance = models.OneToOneField('CommandInstance')
+    lastrun = models.DateTimeField()
+    frequency = models.IntegerField()  # time to wait in seconds
+
+
+class Task(TimeStampedModel):
+    commandinstance = models.OneToOneField('CommandInstance')
+
+
+class CommandInstance(models.Model):
+    command = models.ForeignKey('Command')
+    args = models.TextField()
+    kwargs = models.TextField()
+
+
+class Command(TimeStampedModel):
+    code = models.TextField()
+    owner = models.ForeignKey(Developer, null=True, blank=True)
+
+    """LANGUAGE_CHOICES = (
+    (u'
+    language = models.CharField(max_length=1, default='b')
+    codeTextField (%s for args, %(kwarg_name))
+    ownerforeignkey to Developer
+    languageCharField
+    file_responseBool
+    
+    """
